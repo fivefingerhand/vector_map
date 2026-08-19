77cceba4d0ba405d16765acfdfe39fb9ba49c7ba
Delu 3.0 <delu@localhost>
2026-08-19 17:07:58 +0000

    Improve click lookup browser compatibility

--- js/map.js
+++ js/map.js
@@ -165,11 +165,11 @@ map.on("click", (event) => {
 
 function escapeHtml(value) {
   return String(value)
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
 
 function locateOnce() {
